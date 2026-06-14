c9b6cbff77ac5abd6cc46252a915f9febee086f8
Yunn1y <auyunnabrahman@gmail.com>
2026-06-14 19:02:31 +0800

    fix database

--- PurrFect/AdminDashboard.Designer.cs
+++ PurrFect/AdminDashboard.Designer.cs
@@ -38,23 +38,23 @@ namespace PurrFect
             this.groomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.paymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.petToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.Panel = new System.Windows.Forms.FlowLayoutPanel();
             this.WelcomeLabel = new System.Windows.Forms.Label();
-            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.homeToolStripMenuItem,
             this.managementToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(858, 30);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 0, 2);
             this.menuStrip1.Size = new System.Drawing.Size(965, 36);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
@@ -65,20 +65,20 @@ namespace PurrFect
             this.salesToolStripMenuItem,
             this.BestSellingMS});
             this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
-            this.homeToolStripMenuItem.Size = new System.Drawing.Size(64, 26);
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(77, 32);
             this.homeToolStripMenuItem.Text = "Home";
             // 
             // salesToolStripMenuItem
             // 
             this.salesToolStripMenuItem.Name = "salesToolStripMenuItem";
-            this.salesToolStripMenuItem.Size = new System.Drawing.Size(278, 26);
+            this.salesToolStripMenuItem.Size = new System.Drawing.Size(333, 34);
             this.salesToolStripMenuItem.Text = "Sales";
             this.salesToolStripMenuItem.Click += new System.EventHandler(this.salesToolStripMenuItem_Click);
             // 
             // BestSellingMS
             // 
             this.BestSellingMS.Name = "BestSellingMS";
-            this.BestSellingMS.Size = new System.Drawing.Size(278, 26);
+            this.BestSellingMS.Size = new System.Drawing.Size(333, 34);
             this.BestSellingMS.Text = "Best Selling Service Package";
             this.BestSellingMS.Click += new System.EventHandler(this.BestSellingMS_Click);
             // 
@@ -91,36 +91,39 @@ namespace PurrFect
             this.petToolStripMenuItem,
             this.customerToolStripMenuItem});
             this.managementToolStripMenuItem.Name = "managementToolStripMenuItem";
-            this.managementToolStripMenuItem.Size = new System.Drawing.Size(111, 26);
+            this.managementToolStripMenuItem.Size = new System.Drawing.Size(133, 32);
             this.managementToolStripMenuItem.Text = "Management";
             // 
             // bookingToolStripMenuItem
             // 
             this.bookingToolStripMenuItem.Name = "bookingToolStripMenuItem";
-            this.bookingToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
             this.bookingToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.bookingToolStripMenuItem.Text = "Booking";
             // 
             // groomerToolStripMenuItem
             // 
             this.groomerToolStripMenuItem.Name = "groomerToolStripMenuItem";
-            this.groomerToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
             this.groomerToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.groomerToolStripMenuItem.Text = "Groomer";
             // 
             // paymentToolStripMenuItem
             // 
             this.paymentToolStripMenuItem.Name = "paymentToolStripMenuItem";
-            this.paymentToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
             this.paymentToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.paymentToolStripMenuItem.Text = "Payment";
             // 
             // petToolStripMenuItem
             // 
             this.petToolStripMenuItem.Name = "petToolStripMenuItem";
-            this.petToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
             this.petToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.petToolStripMenuItem.Text = "Pet";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.customerToolStripMenuItem.Text = "Customer";
+            this.customerToolStripMenuItem.Click += new System.EventHandler(this.customerToolStripMenuItem_Click_1);
             // 
             // contextMenuStrip1
             // 
@@ -131,13 +134,10 @@ namespace PurrFect
             // Panel
             // 
             this.Panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Panel.Location = new System.Drawing.Point(0, 30);
+            this.Panel.Location = new System.Drawing.Point(0, 36);
             this.Panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Panel.Name = "Panel";
-            this.Panel.Size = new System.Drawing.Size(858, 528);
-            this.Panel.Location = new System.Drawing.Point(0, 36);
-            this.Panel.Name = "Panel";
-            this.Panel.Size = new System.Drawing.Size(965, 661);
+            this.Panel.Size = new System.Drawing.Size(965, 662);
             this.Panel.TabIndex = 2;
             this.Panel.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_Paint);
             // 
@@ -146,27 +146,20 @@ namespace PurrFect
             this.WelcomeLabel.AutoSize = true;
             this.WelcomeLabel.Font = new System.Drawing.Font("Bookman Old Style", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.WelcomeLabel.ForeColor = System.Drawing.Color.HotPink;
-            this.WelcomeLabel.Location = new System.Drawing.Point(133, 262);
+            this.WelcomeLabel.Location = new System.Drawing.Point(150, 328);
             this.WelcomeLabel.Name = "WelcomeLabel";
-            this.WelcomeLabel.Size = new System.Drawing.Size(558, 35);
+            this.WelcomeLabel.Size = new System.Drawing.Size(665, 42);
             this.WelcomeLabel.TabIndex = 4;
             this.WelcomeLabel.Text = "WELCOME TO ADMIN DASHBOARD";
             this.WelcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.WelcomeLabel.Click += new System.EventHandler(this.WelcomeLabel_Click);
             // 
-            // customerToolStripMenuItem
-            // 
-            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
-            this.customerToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.customerToolStripMenuItem.Text = "Customer";
-            this.customerToolStripMenuItem.Click += new System.EventHandler(this.customerToolStripMenuItem_Click_1);
-            // 
             // AdminDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(858, 558);
+            this.ClientSize = new System.Drawing.Size(965, 698);
             this.Controls.Add(this.WelcomeLabel);
             this.Controls.Add(this.Panel);
             this.Controls.Add(this.menuStrip1);
