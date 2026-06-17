ebaba3e8874a3807085dfd03a62b4c8c1c19aa7f
abslysha <abbyeylysha@gmail.com>
2026-06-17 16:15:48 +0800

    add all update files

--- PurrFect/RegisterPetForm.Designer.cs
+++ PurrFect/RegisterPetForm.Designer.cs
@@ -28,12 +28,342 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelType = new System.Windows.Forms.Label();
+            this.labelBreed = new System.Windows.Forms.Label();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.labelBirthday = new System.Windows.Forms.Label();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxBreed = new System.Windows.Forms.TextBox();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.groupBoxDetails = new System.Windows.Forms.GroupBox();
+            this.pictureBoxPet = new System.Windows.Forms.PictureBox();
+            this.openFileDialogUpload = new System.Windows.Forms.OpenFileDialog();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.groupBoxPhoto = new System.Windows.Forms.GroupBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBoxDetails.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPet)).BeginInit();
+            this.groupBoxPhoto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(6, 49);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(192, 45);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Pet Name :";
+            // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelType.Location = new System.Drawing.Point(30, 131);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(177, 45);
+            this.labelType.TabIndex = 1;
+            this.labelType.Text = "Pet Type :";
+            // 
+            // labelBreed
+            // 
+            this.labelBreed.AutoSize = true;
+            this.labelBreed.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBreed.Location = new System.Drawing.Point(75, 229);
+            this.labelBreed.Name = "labelBreed";
+            this.labelBreed.Size = new System.Drawing.Size(132, 45);
+            this.labelBreed.TabIndex = 2;
+            this.labelBreed.Text = "Breed :";
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGender.Location = new System.Drawing.Point(54, 319);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(153, 45);
+            this.labelGender.TabIndex = 3;
+            this.labelGender.Text = "Gender :";
+            // 
+            // labelBirthday
+            // 
+            this.labelBirthday.AutoSize = true;
+            this.labelBirthday.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBirthday.Location = new System.Drawing.Point(7, 404);
+            this.labelBirthday.Name = "labelBirthday";
+            this.labelBirthday.Size = new System.Drawing.Size(200, 45);
+            this.labelBirthday.TabIndex = 4;
+            this.labelBirthday.Text = "Birth Date :";
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWeight.Location = new System.Drawing.Point(52, 490);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(155, 45);
+            this.labelWeight.TabIndex = 5;
+            this.labelWeight.Text = "Weight :";
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelColor.Location = new System.Drawing.Point(75, 576);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(123, 45);
+            this.labelColor.TabIndex = 6;
+            this.labelColor.Text = "Color :";
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Items.AddRange(new object[] {
+            "Cat",
+            "Dog",
+            "Rabbit",
+            "Bird",
+            "Hamster",
+            "Fish",
+            "Others"});
+            this.comboBoxType.Location = new System.Drawing.Point(228, 144);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(277, 37);
+            this.comboBoxType.TabIndex = 7;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(228, 49);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(493, 53);
+            this.textBoxName.TabIndex = 8;
+            // 
+            // textBoxBreed
+            // 
+            this.textBoxBreed.Location = new System.Drawing.Point(228, 229);
+            this.textBoxBreed.Name = "textBoxBreed";
+            this.textBoxBreed.Size = new System.Drawing.Size(277, 35);
+            this.textBoxBreed.TabIndex = 9;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMale.Location = new System.Drawing.Point(230, 325);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(109, 39);
+            this.radioButtonMale.TabIndex = 10;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(230, 415);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(413, 31);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFemale.Location = new System.Drawing.Point(414, 325);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(137, 39);
+            this.radioButtonFemale.TabIndex = 13;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(228, 504);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(111, 38);
+            this.numericUpDown1.TabIndex = 14;
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.Location = new System.Drawing.Point(228, 589);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(277, 35);
+            this.textBoxColor.TabIndex = 15;
+            // 
+            // groupBoxDetails
+            // 
+            this.groupBoxDetails.BackColor = System.Drawing.Color.Pink;
+            this.groupBoxDetails.Controls.Add(this.textBoxColor);
+            this.groupBoxDetails.Controls.Add(this.numericUpDown1);
+            this.groupBoxDetails.Controls.Add(this.radioButtonFemale);
+            this.groupBoxDetails.Controls.Add(this.dateTimePicker1);
+            this.groupBoxDetails.Controls.Add(this.radioButtonMale);
+            this.groupBoxDetails.Controls.Add(this.textBoxBreed);
+            this.groupBoxDetails.Controls.Add(this.textBoxName);
+            this.groupBoxDetails.Controls.Add(this.comboBoxType);
+            this.groupBoxDetails.Controls.Add(this.labelColor);
+            this.groupBoxDetails.Controls.Add(this.labelWeight);
+            this.groupBoxDetails.Controls.Add(this.labelBirthday);
+            this.groupBoxDetails.Controls.Add(this.labelGender);
+            this.groupBoxDetails.Controls.Add(this.labelBreed);
+            this.groupBoxDetails.Controls.Add(this.labelType);
+            this.groupBoxDetails.Controls.Add(this.labelName);
+            this.groupBoxDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxDetails.Location = new System.Drawing.Point(53, 64);
+            this.groupBoxDetails.Name = "groupBoxDetails";
+            this.groupBoxDetails.Size = new System.Drawing.Size(773, 669);
+            this.groupBoxDetails.TabIndex = 16;
+            this.groupBoxDetails.TabStop = false;
+            this.groupBoxDetails.Text = "Pet Details";
+            // 
+            // pictureBoxPet
+            // 
+            this.pictureBoxPet.Location = new System.Drawing.Point(81, 52);
+            this.pictureBoxPet.Name = "pictureBoxPet";
+            this.pictureBoxPet.Size = new System.Drawing.Size(445, 386);
+            this.pictureBoxPet.TabIndex = 17;
+            this.pictureBoxPet.TabStop = false;
+            // 
+            // openFileDialogUpload
+            // 
+            this.openFileDialogUpload.FileName = "openFileDialog1";
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpload.Location = new System.Drawing.Point(217, 454);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(207, 43);
+            this.buttonUpload.TabIndex = 18;
+            this.buttonUpload.Text = "Upload Image";
+            this.buttonUpload.UseVisualStyleBackColor = false;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // groupBoxPhoto
+            // 
+            this.groupBoxPhoto.BackColor = System.Drawing.Color.Pink;
+            this.groupBoxPhoto.Controls.Add(this.buttonUpload);
+            this.groupBoxPhoto.Controls.Add(this.pictureBoxPet);
+            this.groupBoxPhoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxPhoto.Location = new System.Drawing.Point(939, 64);
+            this.groupBoxPhoto.Name = "groupBoxPhoto";
+            this.groupBoxPhoto.Size = new System.Drawing.Size(607, 517);
+            this.groupBoxPhoto.TabIndex = 19;
+            this.groupBoxPhoto.TabStop = false;
+            this.groupBoxPhoto.Text = "Pet Photo";
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRegister.Location = new System.Drawing.Point(996, 605);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(242, 53);
+            this.buttonRegister.TabIndex = 19;
+            this.buttonRegister.Text = "Register Pet";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.IndianRed;
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(1291, 605);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(242, 53);
+            this.buttonCancel.TabIndex = 20;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.BackColor = System.Drawing.Color.Khaki;
+            this.buttonNext.Location = new System.Drawing.Point(1418, 717);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(224, 81);
+            this.buttonNext.TabIndex = 21;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = false;
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.BackColor = System.Drawing.Color.Khaki;
+            this.buttonPrev.Location = new System.Drawing.Point(1173, 717);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(224, 81);
+            this.buttonPrev.TabIndex = 22;
+            this.buttonPrev.Text = "Previous";
+            this.buttonPrev.UseVisualStyleBackColor = false;
+            // 
+            // RegisterPetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(1654, 970);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.groupBoxPhoto);
+            this.Controls.Add(this.groupBoxDetails);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "RegisterPetForm";
             this.Text = "RegisterPetForm";
+            this.Load += new System.EventHandler(this.RegisterPetForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBoxDetails.ResumeLayout(false);
+            this.groupBoxDetails.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPet)).EndInit();
+            this.groupBoxPhoto.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.Label labelBreed;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.Label labelBirthday;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxBreed;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBoxColor;
+        private System.Windows.Forms.GroupBox groupBoxDetails;
+        private System.Windows.Forms.PictureBox pictureBoxPet;
+        private System.Windows.Forms.OpenFileDialog openFileDialogUpload;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.GroupBox groupBoxPhoto;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrev;
     }
 }
--- PurrFect/RegisterPetForm.cs
+++ PurrFect/RegisterPetForm.cs
@@ -16,5 +16,86 @@ namespace PurrFect
         {
             InitializeComponent();
         }
+
+        private void RegisterPetForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+            openFileDialogUpload.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialogUpload.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxPet.Image = Image.FromFile(openFileDialogUpload.FileName);
+            }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            if (textBoxName.Text == "")
+            {
+                MessageBox.Show("Please enter the pet's name.");
+                return;
+            }
+
+            if (comboBoxType.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select the pet's type.");
+                return;
+            }
+
+            if (textBoxBreed.Text == "")
+            {
+                MessageBox.Show("Please enter the pet's breed.");
+                return;
+            }
+
+            if (!radioButtonMale.Checked && !radioButtonFemale.Checked)
+            {
+                MessageBox.Show("Please select gender.");
+                return;
+            }
+
+            if (dateTimePicker1.Value.Date > DateTime.Now)
+            {
+                MessageBox.Show("Please select a valid birth date.");
+                return;
+            }
+
+            if (numericUpDown1.Value <= 0)
+            {
+                MessageBox.Show("Please enter a valid weight.");
+                numericUpDown1.Focus();
+                return;
+            }
+
+            if (textBoxColor.Text == "")
+            {
+                MessageBox.Show("Please enter the pet's color.");
+                return;
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            textBoxName.Clear();
+            textBoxBreed.Clear();
+            textBoxColor.Clear();
+
+            comboBoxType.SelectedIndex = -1;
+
+            radioButtonFemale.Checked = false;
+            radioButtonMale.Checked = false;
+
+            dateTimePicker1.Value = DateTime.Now;
+
+            numericUpDown1.Value = 0;
+
+            pictureBoxPet.Image = null;
+
+            textBoxName.Focus();
+        }
     }
 }
